c8fd497543379f86339aa3d3288db7cc235b728f
Julio Barrios <juliobarrios0296@gmail.com>
2024-05-27 23:08:34 -0500

    develop-jb: Audio synchronization with guide animation

--- Assets/Scripts/Game/MainCharacter/StartMovement.cs
+++ Assets/Scripts/Game/MainCharacter/StartMovement.cs
@@ -7,9 +7,17 @@ public class StartMovement : MonoBehaviour
     public Animator animator; // Referencia al Animator
     public int layerIndex1;
     public int layerIndex2;
+    public AudioClip birdSongSoundEffectClip;
+    public AudioClip displacementSoundEffectClip;
+    public AudioClip talkSoundEffectClip;
+    private AudioSource audioSource;
 
     void Start()
     {
+		//Sound
+	    audioSource = GetComponent<AudioSource>();
+	    
+	    //Animation
         animator.SetLayerWeight(layerIndex1, 1f);
         animator.SetLayerWeight(layerIndex2, 2f);
     }
@@ -18,12 +26,49 @@ public class StartMovement : MonoBehaviour
     {
 		ControlAnimationPartameters();
     }
+
+    void AudioTalkController()
+    {
+	    if (audioSource.clip == talkSoundEffectClip && audioSource.isPlaying)
+	    {
+		    animator.SetInteger("Actions", 1);
+	    }
+    }
+
+    void InitialMovementClipPlay()
+    {
+	    audioSource.clip = birdSongSoundEffectClip;
+	    audioSource.Play();
+    }
+    
+
 	
 	void ControlAnimationPartameters()
 	{
-		if (animator.GetCurrentAnimatorStateInfo(0).IsName("StartMovement") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1 && !animator.IsInTransition(0))
+		//Animation StartMovement Start
+		//if (animator.GetCurrentAnimatorStateInfo(0).IsName("StartMovement") && 
+		//    animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 1.0f)
+		//{
+//
+//
+		//}
+		
+		//Animation StartMovement End
+		if (animator.GetCurrentAnimatorStateInfo(0).IsName("StartMovement") && 
+		    animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1 && 
+		    !animator.IsInTransition(0))
 		{
+			//Audio
+			audioSource.Pause();
+			audioSource.clip = talkSoundEffectClip;
+			//Animation
 			animator.SetInteger("Actions", 1);
+			audioSource.Play();
 		}
     }
+
+	//void ControlAudioParameters()
+	//{
+	//	
+	//}
 }
