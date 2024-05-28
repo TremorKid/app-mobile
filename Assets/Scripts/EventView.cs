a574bf419e28cf783ca6e19c3709f265d4e2d7eb
EcoCharlie <erikc.cortezb@gmail.com>
2024-05-27 21:02:40 -0500

    Developer-EC: Image Found Added

---
+++ Assets/Scripts/EventView.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class EventView : MonoBehaviour
+{
+    public void ModelFound(string modelName)
+    {
+        Debug.Log(modelName);
+        Debug.Log("Esto es lo que sale gaaaaaaa");
+    }
+}
