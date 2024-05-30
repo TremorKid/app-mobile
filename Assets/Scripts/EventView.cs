19a273410ad95adfbfc2be0fcf9d9bdff8f862ff
EcoCharlie <erikc.cortezb@gmail.com>
2024-05-29 19:42:09 -0500

    Developer-ec: All Model Image Added

--- Assets/Scripts/EventView.cs
+++ Assets/Scripts/EventView.cs
@@ -5,6 +5,5 @@ public class EventView : MonoBehaviour
     public void ModelFound(string modelName)
     {
         Debug.Log(modelName);
-        Debug.Log("Esto es lo que sale gaaaaaaa");
     }
 }
